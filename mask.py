0c67fc7fcc3363ad244f62c773885df54ed42315
rtakatsu1221 <clap.clap.1221@gmail.com>
2021-04-29 22:55:50 -0400

    Update mask.py

--- mask.py
+++ mask.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 G = nx.watts_strogatz_graph(n = 10000, k = 4, p = 0.5)
 G_sf = nx.to_undirected(nx.scale_free_graph(n = 10000))
 G_gr = nx.watts_strogatz_graph(n = 10000, k = 4, p = 0)
+G_r = nx.watts_strogatz_graph(n = 10000, k = 4, p = 1)
 
 colors = ['blue', 'red']
 
@@ -28,9 +29,13 @@ def initGame(graph, thres):
         graph.nodes[node]['strategy'] = strategy
         graph.nodes[node]['payoff'] = 0
         graph.nodes[node]['infected'] = 0
+        graph.nodes[node]['symptom'] = 0 # symptomatic agents don't play
         if (np.random.uniform(0, 1) < 0.001): 
             graph.nodes[node]['infected'] = 14
-            
+            if (np.random.uniform(0, 1) < (1/6)):
+                graph.nodes[node]['symptom'] = 1 
+                graph.nodes[node]['payoff'] = -100 
+                graph.nodes[node]['strategy'] = 0
 
 def getPayoff(graph, node, C, cumulative):
     if (cumulative == 0):
@@ -41,30 +46,46 @@ def getPayoff(graph, node, C, cumulative):
                 graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] + 1
                 if (graph.nodes[node]['infected'] == 0 and 
                     graph.nodes[v]['infected'] > 0 and
+                    graph.nodes[v]['symptom'] == 0 and
                     np.random.uniform(0, 1) < 0.05): # change the param here
                     graph.nodes[node]['infected'] = 14
-                    graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] - 100
+                    if (np.random.uniform(0, 1) < (1/6)):
+                        graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] - 100
+                        graph.nodes[node]['symptom'] = 1 
+                        graph.nodes[node]['strategy'] = 0
             else:
                 graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] - C
                 if (graph.nodes[node]['infected'] == 0 and 
                     graph.nodes[v]['infected'] > 0 and
+                    graph.nodes[v]['symptom'] == 0 and
                     np.random.uniform(0, 1) < 0.25): # change the param here
                     graph.nodes[node]['infected'] = 14
-                    graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] - 100
+                    if (np.random.uniform(0, 1) < (1/6)):
+                        graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] - 100
+                        graph.nodes[node]['symptom'] = 1 
+                        graph.nodes[node]['strategy'] = 0
         else:
             if (graph.nodes[v]['strategy'] == 0):
                 graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] + 1 + C
                 if (graph.nodes[node]['infected'] == 0 and 
                     graph.nodes[v]['infected'] > 0 and
+                    graph.nodes[v]['symptom'] == 0 and
                     np.random.uniform(0, 1) < 0.10): # change the param here
                     graph.nodes[node]['infected'] = 14
-                    graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] - 100
+                    if (np.random.uniform(0, 1) < (1/6)):
+                        graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] - 100
+                        graph.nodes[node]['symptom'] = 1 
+                        graph.nodes[node]['strategy'] = 0
             else:
                 if (graph.nodes[node]['infected'] == 0 and 
                     graph.nodes[v]['infected'] > 0 and
+                    graph.nodes[v]['symptom'] == 0 and
                     np.random.uniform(0, 1) < 0.5): # change the param here
                     graph.nodes[node]['infected'] = 14
-                    graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] - 100
+                    if (np.random.uniform(0, 1) < (1/6)):
+                        graph.nodes[node]['payoff'] = graph.nodes[node]['payoff'] - 100
+                        graph.nodes[node]['symptom'] = 1 
+                        graph.nodes[node]['strategy'] = 0
     
 def simulate(niter, C, graph, thres, cumulative):
     initGame(graph, thres) 
@@ -84,20 +105,30 @@ def simulate(niter, C, graph, thres, cumulative):
         totalInfected.append(infectedCount/len(graph.nodes()))
 
         for node in graph.nodes():
-            getPayoff(graph, node, C, cumulative)
+            if (graph.nodes[node]['symptom'] == 0):
+                getPayoff(graph, node, C, cumulative)
         
-        for node in graph.nodes():
-            maxPayoff = graph.nodes[node]['payoff']
-            maxStrat = graph.nodes[node]['strategy']
-            for v in list(graph.adj[node]):
-                if (graph.nodes[v]['payoff'] > maxPayoff):
-                    maxPayoff = graph.nodes[v]['payoff']
-                    maxStrat = graph.nodes[v]['strategy']
-            if (maxPayoff > graph.nodes[node]['payoff']):
-                graph.nodes[node]['strategy'] = maxStrat # deterministically change
-        
-        graph.nodes[np.random.randint(len(graph.nodes()))]['strategy'] = abs(graph.nodes[np.random.randint(len(graph.nodes()))]['strategy'] - 1)
+        if (i % 7 == 0):
+            for node in graph.nodes(): # can we make this update less frequent ?
+                if (graph.nodes[node]['symptom'] == 0):
+                    maxNode = node
+                    maxPayoff = graph.nodes[node]['payoff']
+                    maxStrategy = graph.nodes[node]['strategy']
+                    for v in list(graph.adj[node]):
+                        if (graph.nodes[v]['symptom'] == 0 and graph.nodes[v]['payoff'] > maxPayoff):
+                            maxPayoff = graph.nodes[v]['payoff']
+                            maxStrategy = graph.nodes[v]['strategy']
+                            maxNode = v
+                    if (maxPayoff > graph.nodes[node]['payoff']):
+                        if (np.random.uniform(0, 1) < (graph.nodes[maxNode]['payoff'] - graph.nodes[node]['payoff'])/(max(len(list(graph.adj[node])), len(list(graph.adj[maxNode])))*(1+C))):
+                            graph.nodes[node]['strategy'] = maxStrategy
+                       
+            for node in graph.nodes():
+                graph.nodes[node]['payoff'] = 0
+                       
             
+        graph.nodes[np.random.randint(len(graph.nodes()))]['strategy'] = abs(graph.nodes[np.random.randint(len(graph.nodes()))]['strategy'] - 1)
+        graph.nodes[np.random.randint(len(graph.nodes()))]['infected'] = 14
         
 # ==choosing single vertex to update===========================================
 #         u = np.random.randint(len(graph.nodes()))
@@ -116,62 +147,47 @@ def simulate(niter, C, graph, thres, cumulative):
         
     return (mask, totalInfected) 
         
-maskSW = simulate(50000, 0.7, G, 0.25, 0)
-maskSW2 = simulate(50000, 0.7, G, 0.5, 0)
-maskSW4 = simulate(50000, 0.7, G, 0.75, 0)
-maskSW5 = simulate(50000, 0.7, G, 0.85, 0)
-maskSW3 = simulate(50000, 0.7, G, 0.9, 0)
-plt.figure(figsize = (8, 8))
-plt.title("SINGLE PAYOFF")
-plt.plot(range(50000), maskSW) 
-plt.plot(range(50000), maskSW2) 
-plt.plot(range(50000), maskSW4) 
-plt.plot(range(50000), maskSW5)
-plt.plot(range(50000), maskSW3) 
-
-maskSW = simulate(50000, 0.5, G, 0.25, 1)
-maskSW2 = simulate(50000, 0.5, G, 0.5, 1)
-maskSW4 = simulate(50000, 0.5, G, 0.75,1)
-maskSW5 = simulate(50000, 0.5, G, 0.85, 1)
-maskSW3 = simulate(50000, 0.5, G, 0.9, 1)
-plt.figure(figsize = (8, 8))
-plt.title("CUMULATIVE")
-plt.plot(range(50000), maskSW) 
-plt.plot(range(50000), maskSW2) 
-plt.plot(range(50000), maskSW4) 
-plt.plot(range(50000), maskSW5)
-plt.plot(range(50000), maskSW3) 
-plt.figure(figsize = (12, 12))
-nx.draw(G, node_size = 50, node_color=[colors[G.nodes[node]['strategy']] for node in G.nodes])
+nx.draw(G_gr, node_size = 50, node_color=[colors[G.nodes[node]['strategy']] for node in G.nodes])
 
 
-maskSF = simulate(200000, 1.9, 0.7, G_sf, 0.25)
-maskSF2 = simulate(50000, 0.5, G_sf, 0.5, 1)
-maskSF4 = simulate(50000, 0.5, G_sf, 0.75, 1)
-maskSF5 = simulate(200000, 1.9, 0.7, G_sf, 0.85)
-maskSF3 = simulate(200000, 1.9, 0.7, G_sf, 0.9)
-plt.figure(figsize = (8, 8))
-plt.plot(range(200000), maskSF) 
-plt.plot(range(200000), maskSF2) 
-plt.plot(range(200000), maskSF4) 
-plt.plot(range(200000), maskSF5)
-plt.plot(range(200000), maskSF3) 
-
-maskSW = simulate(1000, 0.5, G, 0.5, 1)
-maskSF = simulate(1000, 0.5, G_sf, 0.5, 1)
-maskGR = simulate(1000, 0.5, G_gr, 0.5, 1)
+maskSW = simulate(10000, 0.5, G, 0.5, 1)
+maskSF = simulate(10000, 0.5, G_sf, 0.5, 1)
+maskGR = simulate(10000, 0.5, G_gr, 0.5, 1)
+maskR = simulate(10000, 0.5, G_r, 0.5, 1)
 
 plt.figure(figsize = (8, 8))
-plt.title("Percentage of people wearing masks")
-plt.plot(range(1000), maskSW[0], color="green")
-plt.plot(range(1000), maskSF[0], color="skyblue")
-plt.plot(range(1000), maskGR[0], color="coral")
+plt.title("People wearing masks (%)")
+plt.plot(range(10000), [100*i for i in maskSW[0]], color="green", label="Small World")
+plt.plot(range(10000), [100*i for i in maskSF[0]], color="skyblue", label="Scale Free")
+plt.plot(range(10000), [100*i for i in maskGR[0]], color="coral", label="Grid")
+plt.plot(range(10000), [100*i for i in maskR[0]], color="pink", label="Random")
+plt.legend()
+plt.ylim(0,100)
 
 plt.figure(figsize = (8, 8))
-plt.title("Percentage of total infected")
-plt.plot(range(1000), maskSW[1], color="green", label="Small World")
-plt.plot(range(1000), maskSF[1], color="skyblue", label="Scale Free")
-plt.plot(range(1000), maskGR[1], color="coral", label="Random Graph")
+plt.title("Infected (%)")
+plt.plot(range(10000), [100*i for i in maskSW[1]], color="green", label="Small World")
+plt.plot(range(10000), [100*i for i in maskSF[1]], color="skyblue", label="Scale Free")
+plt.plot(range(10000), [100*i for i in maskGR[1]], color="coral", label="Grid")
+plt.plot(range(10000), [100*i for i in maskR[1]], color="pink", label="Random")
+plt.legend()
+plt.ylim(0,100)
+
+maskSWsinglesim = maskSW
+maskSFsinglesim = maskSF
+maskGRsinglesim = maskGR
+maskRsinglesim = maskR
+
+maskSWsingledetsim = maskSW
+maskSFsingledetsim = maskSF
+maskGRsingledetsim = maskGR
+maskRsingledetsim = maskR
+
+maskSW7daysdetsim = maskSW
+maskSF7daysdetsim = maskSF
+maskGR7daysdetsim = maskGR
+maskR7daysdetsim = maskR
+
 
 
 
